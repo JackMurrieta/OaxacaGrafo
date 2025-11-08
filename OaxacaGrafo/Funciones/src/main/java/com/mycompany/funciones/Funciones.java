ec736e8bf2f76c591ed4a14483e41c0b9c3264d1
JackMurrieta <jack.murrieta252851@potros.itson.edu.mx>
2025-11-07 21:46:48 -0700

    Estructura proyecto
    ahi vamos viendo

---
+++ OaxacaGrafo/Funciones/src/main/java/com/mycompany/funciones/Funciones.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.funciones;
+
+/**
+ *
+ * @author Jack Murrieta
+ */
+public class Funciones {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ OaxacaGrafo/Presentacion/src/main/java/com/mycompany/presentacion/Presentacion.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.presentacion;
+
+/**
+ *
+ * @author Jack Murrieta
+ */
+public class Presentacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
